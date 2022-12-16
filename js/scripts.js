ae84aba6eb167b8f16feaec4c817cd0699be72a3
Fedois <fedefox14@gmail.com>
2022-12-16 15:22:06 +0100

    fine progetto

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+alert('clicca su "ok" per avviare il generatore di password');
+
+const nome = prompt('inserisci qui il tuo nome');
+console.log('nome utente: ', nome );
+
+const cognome = prompt('inserisci qui il tuo cognome');
+console.log('cognome utente: ', cognome );
+
+const colore = prompt('inserisci qui il tuo colore preferito');
+console.log('colore preferito utente: ', colore );
+
+console.log('riassunto utente: ', nome, cognome, colore)
+
+document.writeln('la tua password è: ', nome, cognome, colore, '21')
+
+
+
+
+
+
+
+
+
+
